1c7bce3cc13ff2a0bfcf42ea8f568b401480a006
bourn404 <bourn404@gmail.com>
2019-01-31 08:28:44 -0700

    Set up npm and Bulma

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Anno</title>
+    <link rel="stylesheet" href="/assets/stylesheets/compiled/main.css">
+</head>
+<body>
+
+</body>
+</html>
